8ea1d93f46ad4a6a43251c7cc01678e42625a1af
bennyp11 <ben.r.pollock@gmail.com>
2023-03-06 21:06:04 -0800

    confirmed data receival

--- src/main/java/com/springapp/demo/Controllers/BookingController.java
+++ src/main/java/com/springapp/demo/Controllers/BookingController.java
@@ -13,8 +13,18 @@ public class BookingController {
 	@PostMapping("/book-cab")
 	public String bookCab(@ModelAttribute Booking booking) {
 	    // Save the booking data to the database or send it to an external service for processing
+
+	    // Print the booking data to the console
+	    System.out.println("Booking data received:");
+	    System.out.println("Pickup Location: " + booking.getPickup());
+	    System.out.println("Destination: " + booking.getDestination());
+	    System.out.println("Date: " + booking.getDate());
+	    System.out.println("Time: " + booking.getTime());
+	    System.out.println("Passengers: " + booking.getPassengers());
+
 	    // Redirect to a confirmation page or display a success message in the current page
 	    return "redirect:/confirmation";
 	}
+
 
 }
