807819a5d82c3948df226fd623bca383bc8a4f02
bennyp11 <ben.r.pollock@gmail.com>
2023-03-06 21:02:24 -0800

    fixed /bookings endpoint issue

--- src/main/java/com/springapp/demo/Controllers/BookingController.java
+++ src/main/java/com/springapp/demo/Controllers/BookingController.java
@@ -1,6 +1,5 @@
 package com.springapp.demo.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +9,6 @@ import com.springapp.demo.models.Booking;
 @RestController
 public class BookingController {
 
-   // Remove this method since it's not needed
-   // @GetMapping("/bookings")
-   // public String getBookings() {
-   //    // This method should return the booking data in a format that can be displayed in the HTML template
-   //    return "Booking data 123456789";
-   // }
-   
    // Add this method to handle the form submission
 	@PostMapping("/book-cab")
 	public String bookCab(@ModelAttribute Booking booking) {
