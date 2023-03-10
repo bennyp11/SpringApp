939f43d225147d44fbcc3243cd17895810f27c85
bennyp11 <ben.r.pollock@gmail.com>
2023-03-09 21:39:23 -0800

    added booking microservice and got it to work with cabfareservice. cab fare is console.logged

--- src/main/java/com/springapp/demo/Controllers/BookingController.java
+++ src/main/java/com/springapp/demo/Controllers/BookingController.java
@@ -1,30 +1,34 @@
 package com.springapp.demo.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springapp.demo.Models.Booking;
+import com.springapp.demo.Services.BookingService;
 
 @RestController
 public class BookingController {
+    
+    @Autowired
+    private BookingService bookingService;
 
-   // Add this method to handle the form submission
-	@PostMapping("/book-cab")
-	public String bookCab(@ModelAttribute Booking booking) {
-	    // Save the booking data to the database or send it to an external service for processing
+    // Add this method to handle the form submission
+    @PostMapping("/book-cab")
+    public String bookCab(@ModelAttribute Booking booking) {
+        // Save the booking data to the database or send it to an external service for processing
 
-	    // Print the booking data to the console
-	    System.out.println("Booking data received:");
-	    System.out.println("Pickup Location: " + booking.getPickup());
-	    System.out.println("Destination: " + booking.getDestination());
-	    System.out.println("Date: " + booking.getDate());
-	    System.out.println("Time: " + booking.getTime());
-	    System.out.println("Passengers: " + booking.getPassengers());
+        try {
+            // Calculate the fare based on the pickup and destination addresses
+            bookingService.bookCab(booking.getPickup(), booking.getDestination());
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception
+        }
 
-	    // Redirect to a confirmation page or display a success message in the current page
-	    return "redirect:/confirmation";
-	}
-
-
+        // Redirect to a confirmation page or display a success message in the current page
+        return "redirect:/confirmation";
+    }
 }
+
---
+++ src/main/java/com/springapp/demo/Services/BookingService.java
@@ -0,0 +1,19 @@
+package com.springapp.demo.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+    
+    @Autowired
+    private CabFareService cabFareService;
+
+    public void bookCab(String pickupAddress, String destinationAddress) throws Exception {
+        // Calculate the fare based on the pickup and destination addresses
+        double fare = cabFareService.calculateFare(pickupAddress, destinationAddress);
+
+        // Print the fare to the console
+        System.out.println("Cab fare: $" + fare);
+    }
+}
